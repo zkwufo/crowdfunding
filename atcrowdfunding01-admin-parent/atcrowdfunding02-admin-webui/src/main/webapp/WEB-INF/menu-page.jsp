f8001c945fc522ac76a49785a025cee7cb690490
zkwufo <zkwufo@gmail.com>
2020-12-15 21:03:58 +0900

    commit

--- atcrowdfunding01-admin-parent/atcrowdfunding02-admin-webui/src/main/webapp/WEB-INF/menu-page.jsp
+++ atcrowdfunding01-admin-parent/atcrowdfunding02-admin-webui/src/main/webapp/WEB-INF/menu-page.jsp
@@ -8,7 +8,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <!DOCTYPE html>
 <html lang="zh-CN">
-<%@include file="/WEB-INF/include-head.jsp"%>
+<%@include file="/WEB-INF/include-head.jsp" %>
 <%--导入ztree的css文件--%>
 <link rel="stylesheet" href="ztree/zTreeStyle.css"/>
 <%--导入ztree的js文件--%>
@@ -16,47 +16,24 @@
 <script type="text/javascript" src="crowd/my-menu.js"></script>
 <script type="text/javascript">
     $(function () {
-        //1 准备生成树形结构的JSON数据，数据的来源是发送ajax请求得到
-        $.ajax({
-           "url":"menu/get/whole/tree.json",
-            "type":"post",
-            "dataType":"json",
-            "success":function (response) {
-                var result = response.result;
-                if(result == "SUCCESS"){
-                //2 创建一个json对象用于存放对ztree做的各种设置
-                    var setting = {
-                        "view":{
-                            addDiyDom: myAddDiyDom
-                        }
-                    };
-
-                //3 从响应体中获取用来生成树形结构的JSON数据。
-                    var zNodes = response.data;
-                //4 初始化树形结构
-                $.fn.zTree.init($("#treeDemo"),setting,zNodes);
-                }
-                if(result =="FAILED"){
-                    layer.msg(response.message);
-                }
-            },
-            "error":function (response) {
-                layer.msg(response.status + " " + response.statusText);
-            },
-        });
+        //调用专门封装好的函数初始化树形结构
+        generateTree();
     })
 </script>
 <body>
-<%@include file="/WEB-INF/include-nav.jsp"%>
+<%@include file="/WEB-INF/include-nav.jsp" %>
 <div class="container-fluid">
     <div class="row">
-        <%@include file="/WEB-INF/include-sidebar.jsp"%>
+        <%@include file="/WEB-INF/include-sidebar.jsp" %>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
 
             <div class="panel panel-default">
-                <div class="panel-heading"><i class="glyphicon glyphicon-th-list"></i> 权限菜单列表 <div style="float:right;cursor:pointer;" data-toggle="modal" data-target="#myModal"><i class="glyphicon glyphicon-question-sign"></i></div></div>
+                <div class="panel-heading"><i class="glyphicon glyphicon-th-list"></i> 权限菜单列表
+                    <div style="float:right;cursor:pointer;" data-toggle="modal" data-target="#myModal"><i
+                            class="glyphicon glyphicon-question-sign"></i></div>
+                </div>
                 <div class="panel-body">
-<%--                    这个ul标签是ztree动态生成的节点所依附的静态节点--%>
+                    <%--                    这个ul标签是ztree动态生成的节点所依附的静态节点--%>
                     <ul id="treeDemo" class="ztree"></ul>
                 </div>
             </div>
--- atcrowdfunding01-admin-parent/atcrowdfunding02-admin-webui/src/main/webapp/crowd/my-menu.js
+++ atcrowdfunding01-admin-parent/atcrowdfunding02-admin-webui/src/main/webapp/crowd/my-menu.js
@@ -1,6 +1,45 @@
+//生成树形结构的函数
+function generateTree() {
+    //1 准备生成树形结构的JSON数据，数据的来源是发送ajax请求得到
+    $.ajax({
+        "url": "menu/get/whole/tree.json",
+        "type": "post",
+        "dataType": "json",
+        "success": function (response) {
+            var result = response.result;
+            if (result == "SUCCESS") {
+                //2 创建一个json对象用于存放对ztree做的各种设置
+                var setting = {
+                    view: {
+                        addDiyDom: myAddDiyDom,
+                        addHoverDom: myAddHoverDom,
+                        removeHoverDom: myRemoveHoverDom
+                    },
+                    //data 下的key下的url实现点击不跳转，url给任意不存在的属性名即可
+                    data: {
+                        key: {
+                            url: "dontJump",
+                        },
+                    },
+                };
+
+                //3 从响应体中获取用来生成树形结构的JSON数据。
+                var zNodes = response.data;
+                //4 初始化树形结构
+                $.fn.zTree.init($("#treeDemo"), setting, zNodes);
+            }
+            if (result == "FAILED") {
+                layer.msg(response.message);
+            }
+        },
+        "error": function (response) {
+            layer.msg(response.status + " " + response.statusText);
+        },
+    });
+}
 function myAddDiyDom(treeId, treeNode) {
     //treeId 是整个树形结构附着的ul标签的id
-    console.log("treeid是:" +treeId);
+    console.log("treeid是:" + treeId);
     //当前树形节点的全部的数据，包括从后端查询得到的menu对象的全部属性
     console.log(treeNode);
     //zTree生成id的规则
@@ -8,10 +47,61 @@ function myAddDiyDom(treeId, treeNode) {
     //解析：ul标签的id_当前节点的序号_功能
     // 提示：“ul标签的id_当前节点的序号”部分可以通过访问treeNode的tId属性得到
     //根据id生成规则拼接出来span标签的id
-    var spanId = treeNode.tId+"_ico";
+    var spanId = treeNode.tId + "_ico";
     //根据控制图标的span标签的id找到这个span标签
     //删除旧的class添加新的class
-    $("#"+spanId)
+    $("#" + spanId)
         .removeClass()
         .addClass(treeNode.icon);
+}
+
+function myAddHoverDom(treeId, treeNode) {
+    //按钮的标签结构<span><a><i></i></a><a><i></i></a></span>
+    //按钮组出现的位置：节点中treeDemo_n_a超链接的后面
+    var anchorId = treeNode.tId + "_a";
+    // 为了后续能移除span则给span添加id
+    var btnGroupId = "btnGroup" + treeNode.tId;
+
+    // 判断以前是否已经添加了按钮组
+    if ($("#" + btnGroupId).length > 0) {
+        return;
+    }
+
+    //准备各个按钮的HTML标签
+    var addBtn = "<a id='" + treeNode.id + "' " +
+        "class= 'btn btn-info dropdown-toggle btn-xs' styl='margin-left:10px;padding-top:0px;' href='#' title='添加子节点'>" +
+        "&nbsp;&nbsp;<i class='fa fa-fw fa-plus rbg '></i></a>";
+    var editBtn = "<a id='" + treeNode.id + "' " +
+        "class= 'btn btn-info dropdown-toggle btn-xs' styl='margin-left:10px;padding-top:0px;' href='#' title='修改子节点'>" +
+        "&nbsp;&nbsp;<i class='fa fa-fw fa-edit rbg '></i></a>";
+    var removeBtn = "<a id='" + treeNode.id + "' " +
+        "class= 'btn btn-info dropdown-toggle btn-xs' styl='margin-left:10px;padding-top:0px;' href='#' title='删除子节点'>" +
+        "&nbsp;&nbsp;<i class='fa fa-fw fa-times rbg '></i></a>";
+
+    // 获取当前节点的级别数据
+    var level = treeNode.level;
+    // 声明变量存储拼装好的按钮代码
+    var btnHtml = "";
+    //判断当前节点的级别
+    if (level == 0) {
+        btnHtml = addBtn;
+    }
+    if (level == 1) {
+        btnHtml = addBtn + " " + editBtn;
+        var length = treeNode.children.length;
+        if (length == 0) {
+            btnHtml = btnHtml + " " + removeBtn;
+        }
+    }
+    if (level == 2) {
+        btnHtml = editBtn + " " + removeBtn;
+    }
+
+    //执行在超链接后面附加span元素的操作
+    $("#"+anchorId).after("<span id='"+btnGroupId+"'>"+btnHtml+"</span>")
+}
+
+function myRemoveHoverDom(treeId, treeNode) {
+    var btnGroupId = "btnGroup" + treeNode.tId;
+    $("#" + btnGroupId).remove();
 }
